638661bc2a123aae29458e5138f5f80eed094036
vamsi1609 <yamuisvamsi@outlook.com>
2021-03-24 13:24:57 +0530

    updated get_data.py and created load_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -7,10 +7,22 @@ import pandas as pd
 import argparse 
 
 def read_params(config_path):
+    '''
+    takes the config_path as input and 
+    returns the loaded yaml file 
+    '''
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file) 
     return config
 def getData(config_path):
+    '''
+    Takes the input as config_path and 
+    gives the input to the read_params function
+    which returns the loaded yaml_file 
+    using which this function will create the dataframe
+    of the data using pandas
+    and returns the dataframe
+    '''
     config=read_params(config_path)
     data_path = config["data_source"]["source"]
     df = pd.read_csv(data_path, sep=",", encoding= 'utf-8')
--- src/load_data.py
+++ src/load_data.py
@@ -1,2 +1,26 @@
 ### Loading the data from the data source
 ### Saving it in the data\raw folder for preprocessing
+import os
+from get_data import read_params, getData
+import argparse
+
+def load_n_save(config_path):
+    '''
+    loads the data from the config_path using the 
+    functions from get_data.py file
+    and saves to the data folder
+    '''
+    config = read_params(config_path)
+    df = getData(config_path)
+    # a liitle preprocessing for changing the name of the columns
+    # because the names have spaces between them which can cause issues 
+    # in the future
+    up_cols = [col.replace(" ","_") for col in df.columns] 
+    raw_path = config["load_data"]["raw_dataset"]
+    df.to_csv(raw_path, sep=",", index=False, header=up_cols) 
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    load_n_save(config_path=parsed_args.config)
